3b503a344b64d7b7e2c0f46503e895f1fa014df2
Narcis <narcis.nnp@gmail.com>
2024-10-03 21:19:31 +0300

    added prime numbers solution

---
+++ src/Hello.java
@@ -0,0 +1,71 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        int myFirstVariable = 100;
+        int mySecondVariable = 0;
+        final int MAX_SIZE = 10;
+        final double PI = Math.PI;
+
+        System.out.println("Hello world !");
+
+        System.out.println("Second variable value is " + mySecondVariable);
+
+        myFirstVariable = MAX_SIZE + 1;
+
+        System.out.println(args[0] + args[1]);
+
+        int operandLeft = Integer.parseInt(args[0]);
+        int operandRightt = Integer.parseInt(args[1]);
+        System.out.println(operandLeft + operandRightt);
+
+        String currentDay = "Monday";
+        switch (currentDay){
+            case "Monday":{
+                System.out.println("No Monday");
+                break;
+            }
+            case "Friday":{
+                System.out.println("yeeey Friday");
+                break;
+            }
+            default: {
+                System.out.println("meeeh");
+            }
+        }
+
+        int currentNumber = 1;
+        while(currentNumber <= MAX_SIZE){
+            System.out.println("Current number is" + currentNumber);
+            currentNumber++;
+        }
+
+        currentNumber = 1;
+        do {
+            System.out.println("Current number is" + currentNumber);
+            currentNumber++;
+        }
+        while(currentNumber <= MAX_SIZE);
+
+        for (int i =1; i <= MAX_SIZE; i++){
+            System.out.println("Current number is" + 1);
+
+        }
+
+        for(String arg : args){
+            System.out.println(arg);
+        }
+        for (int i=2; i<=100000; i++){
+            boolean isPrime = true;
+            for (int j=2; j <=i/2; j++){
+                if( i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                System.out.println("Numer" + i + "is prime.");
+            }
+        }
+    }
+}
